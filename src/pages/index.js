4a0e4834a2c3893b5a95d4ffda414ce2bd81f475
Easin Webpro <easinislam299@gmail.com>
2022-10-27 22:54:07 +0600

    [src] :+1: Add layout - fix folder structure & pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
+import { NavBar } from 'components/Layout/NavBar';
+import { Welcome } from 'components/Welcome/Welcome';
+import { ColorSchemeToggle } from 'components/ColorSchemeToggle/ColorSchemeToggle';
+import Layout from 'components/Layout/Layout';
 
 export default function HomePage() {
   return (
     <>
-      <ColorSchemeToggle />
-      <Welcome />
+      <NavBar />
+      <Layout>
+        <Welcome />
+      </Layout>
     </>
   );
 }
