e4893dcbedb2ba979c0a6c8e2b20070aaf2696c1
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2022-10-29 15:07:14 +0600

    fix: change to loop carousel of        categories and dark mood color fix for category.

--- src/components/Categories/Category/Category.style.js
+++ src/components/Categories/Category/Category.style.js
@@ -2,7 +2,10 @@ import { createStyles } from '@mantine/core';
 
 export const categoryStyles = createStyles((theme) => ({
 	wrapper: {
-		backgroundColor: theme.colors.gray[1],
+		backgroundColor:
+			theme.colorScheme === 'dark'
+				? theme.colors.gray[9]
+				: theme.colors.gray[1],
 		padding: '12px 15px',
 		width: '100px',
 		height: '120px',
@@ -11,5 +14,9 @@ export const categoryStyles = createStyles((theme) => ({
 	},
 	text: {
 		marginTop: '5px',
+		color:
+			theme.colorScheme === 'light'
+				? theme.colors.gray[6]
+				: theme.colors.gray[0],
 	},
 }));
