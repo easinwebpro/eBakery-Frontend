c33005ad8e820fb2d080cf429705f90bf69c2ff0
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2022-10-30 06:10:31 +0600

    fix: conflict navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import { Categories } from 'components/Categories';
 import Layout from 'components/Layout/Layout';
-import { NavBar } from 'components/Layout/NavBar';
+import { Services } from 'components/Services';
 import { Welcome } from 'components/Welcome/Welcome';
 
 export default function HomePage() {
 	return (
 		<>
-			<NavBar />
 			<Layout>
 				<Categories />
 				<Welcome />
