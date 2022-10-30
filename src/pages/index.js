9035b3ef6967d33b2d0f639ede95ef21c68276b0
A. F. M Shahriar Sharif (Rahi) <afmssrahi@gmail.com>
2022-10-30 23:11:51 +0600

    fix: double navbar fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { Categories } from 'components/Categories';
 import Layout from 'components/Layout/Layout';
-import { NavBar } from 'components/Layout/NavBar';
 import { HeroSection } from 'components/HeroSection';
 import { Services } from 'components/Services';
 import { Welcome } from 'components/Welcome/Welcome';
@@ -8,7 +7,6 @@ import { Welcome } from 'components/Welcome/Welcome';
 export default function HomePage() {
 	return (
 		<>
-			<NavBar />
 			<Layout>
 				<HeroSection/>
 				<Categories />
