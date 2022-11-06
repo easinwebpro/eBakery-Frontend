9acc27ca89fe0a8f97d816637375a2cfe291145a
Easin Webpro <easinislam299@gmail.com>
2022-11-06 16:51:08 +0600

    :lipstick: [component/Product] add to cart button

---
+++ src/components/Product/AddToCart/add-to-cart.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@mantine/core';
+import { IconBasket, IconMoodSad } from '@tabler/icons';
+import React from 'react'
+
+export const AddToCart = ({ addToCart, disabled = false }) => {
+    return (
+        <div>
+            <Button onClick={() => console.log('Hello!')} disabled={disabled} radius="lg" fullWidth>
+                {disabled !== true ? <IconBasket size={20} stroke={2} /> : <IconMoodSad size={20} stroke={2} />} {disabled !== true ? <span style={{marginLeft: '5px'}}>Add to Cart</span> : <span style={{marginLeft: '5px'}} >Out of Stock</span>}
+            </Button>
+        </div>
+    )
+};
