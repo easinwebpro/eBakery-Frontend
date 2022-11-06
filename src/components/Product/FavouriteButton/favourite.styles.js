3c32d044019bec5c80897431fb6ad19e4737d721
Easin Webpro <easinislam299@gmail.com>
2022-11-06 17:45:39 +0600

    :lipstick: [ component/Favourite ] add favourite button desgin

---
+++ src/components/Product/FavouriteButton/favourite.styles.js
@@ -0,0 +1,26 @@
+const { createStyles } = require("@mantine/styles");
+
+export const useStyles = createStyles((theme) => ({
+
+    favorite_icon: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    favorite_icon_btn: {
+        borderRadius: '50%',
+        height: '35px',
+        width: '35px',
+        border: 'none',
+        background: '#FEE',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+
+    'favorite_icon_color': {
+        color: 'rgb(255, 4, 0)'
+    }
+
+}));
