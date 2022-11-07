67a7f584b67064fa172fa7e93ee0d8c234478ddd
Easin Webpro <easinislam299@gmail.com>
2022-11-07 17:58:13 +0600

    :lipstick: & :fire:  [ compontent/Product & Favourite ]add to cart btn color & favourite cursor pointer add

--- src/components/Product/AddToCart/add-to-cart.jsx
+++ src/components/Product/AddToCart/add-to-cart.jsx
@@ -5,8 +5,8 @@ import { IconBasket, IconMoodSad } from '@tabler/icons';
 export const AddToCart = ({ addToCart, disabled = false }) => {
     return (
         <div>
-            <Button onClick={() => console.log('Hello!')} disabled={disabled} radius="lg" fullWidth>
-                {disabled !== true ? <IconBasket size={20} stroke={2} /> : <IconMoodSad size={20} stroke={2} />} {disabled !== true ? <span style={{marginLeft: '5px'}}>Add to Cart</span> : <span style={{marginLeft: '5px'}} >Out of Stock</span>}
+            <Button onClick={() => console.log('Hello!')} disabled={disabled} radius="lg" fullWidth variant="gradient" gradient={{ from: 'teal', to: 'lime', deg: 105 }}>
+                {disabled !== true ? <IconBasket size={20} stroke={2} /> : <IconMoodSad size={20} stroke={2} />} {disabled !== true ? <span style={{ marginLeft: '5px' }}>Add to Cart</span> : <span style={{ marginLeft: '5px' }} >Out of Stock</span>}
             </Button>
         </div>
     )
