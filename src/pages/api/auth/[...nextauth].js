4904716bfd1621979fb07b1a0b78f17873b20356
Easin Webpro <easinislam299@gmail.com>
2022-11-08 22:36:39 +0600

    :sparkles: [next-auth] setup next-auth and config with custom design & mange auth using api call

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,30 +1,83 @@
+import { login, signup } from "api";
+import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-providers: [
-  CredentialsProvider({
-    // The name to display on the sign in form (e.g. "Sign in with...")
-    name: "Credentials",
-    // `credentials` is used to generate a form on the sign in page.
-    // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-    // e.g. domain, username, password, 2FA token, etc.
-    // You can pass any HTML attribute to the <input> tag through the object.
-    credentials: {
-      username: { label: "Username", type: "text", placeholder: "jsmith" },
-      password: { label: "Password", type: "password" }
-    },
-    async authorize(credentials, req) {
-      // Add logic here to look up the user from the credentials supplied
-      const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
+export const authOptions = {
 
-      if (user) {
-        // Any object returned will be saved in `user` property of the JWT
-        return user
-      } else {
-        // If you return null then an error will be displayed advising the user to check their details.
-        return null
+  secret: process.env.NEXTAUTH_SECRET,
 
-        // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+  providers: [
+
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+
+      async authorize(credentials, req) {
+
+        if (credentials == null) return null;
+        /**
+         * credentials is defined in the config above.
+         * We can expect it contains two properties: `email` and `password`
+         */
+
+        const { type } = credentials;
+
+        if (type === 'register') {
+          try {
+            const { username, email, password } = credentials;
+            const { data } = await signup({ username, email, password });
+            return { jwt: data.jwt, user: data?.user };
+
+          } catch (error) {
+            console.warn(error);
+          }
+
+        } else {
+
+          try {
+            const { email, password } = credentials;
+            const { data } = await login({ identifier: email, password: password });
+            // console.log('Easin Webpro user login', data);
+            return { jwt: data.jwt, user: data?.user };
+          } catch (error) {
+            console.warn(error);
+          }
+        }
+
       }
-    }
-  })
-]
+    })],
+  callbacks: {
+    jwt: async ({ token, user }) => {
+      if (user) {
+        token.id = user.id;
+        token.jwt = user.jwt;
+        token.user = user.user; /* ### */
+      }
+      return token;
+    },
+
+    session: async ({ session, token }) => {
+      session.id = token.id;
+      session.jwt = token.jwt;
+      session.user = token.user;
+      return session;
+    },
+
+  },
+
+  session: {
+    jwt: true
+  },
+
+
+  pages: {
+    signIn: '/auth/login',
+    newUser: '/auth/register',
+    // New users will be directed here on first sign in (leave the property out if not of interest)
+    // signOut: '/auth/signout',
+    error: '/auth/error', // Error code passed in query string as ?error=
+  }
+
+}
+
+export default NextAuth(authOptions);
