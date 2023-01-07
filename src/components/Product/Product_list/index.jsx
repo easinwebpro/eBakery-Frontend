de0b761da773825484e7177bba8457b370f934e0
Easin Webpro <easinislam299@gmail.com>
2023-01-07 23:55:32 +0600

    🚧 [components/products] product_list in progress design

--- src/components/Product/Product_list/index.jsx
+++ src/components/Product/Product_list/index.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react';
 import { createStyles, Table, Checkbox, ScrollArea, Group, Avatar, Text, Center, Space } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import { CartCounter } from '../AddToCart/cart-counter';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { IconX } from '@tabler/icons';
 import { useDispatch } from 'react-redux';
 import { removeItemToCart } from 'store/cartSlice';
+import { Product_Cart } from '../Product_Cart';
 
 const useStyles = createStyles((theme) => ({
     rowSelected: {
@@ -18,6 +20,7 @@ const useStyles = createStyles((theme) => ({
 export const Product_list = ({ products }) => {
 
     const dispatch = useDispatch();
+    const matches = useMediaQuery('(max-width: 766px)');
 
     const { classes, cx } = useStyles();
 
@@ -27,50 +30,60 @@ export const Product_list = ({ products }) => {
 
     const productsKey = Object.keys(products);
 
-    const rows = productsKey.map((key) => {
-
-        return (
-            <tr key={key} >
-                <td onClick={()=> removeItemToCartHandler(key)} style={{cursor:'pointer'}}>
-                    <IconX size={20} stroke={4} color="red" />
-                </td>
-                <td>
-                    <Group spacing="sm">
-                        <LazyLoadImage
-                            style={{ borderRadius: '20px' }}
-                            width="100px"
-                            height="100px"
-                            objectFit="cover"
-                            radius='md'
-                            src={products[key]?.product_image}
-                            effect="blur" />
-
-                        <Text size="sm" weight={700}>
-                            {products[key]?.title}
-                        </Text>
-                    </Group>
-                </td>
-                <td><Center> <Text size="sm" weight={700}>{products[key]?.price} </Text> </Center></td>
-                <td style={{ width: 140 }}><Center><CartCounter product={products[key]} /> </Center></td>
-            </tr>
-        );
-    });
+    // const rows = 
 
     return (
         <ScrollArea>
-            <Table sx={{ minWidth: 800 }} verticalSpacing="sm">
-                <thead>
-                    <tr>
-                        <th>
 
-                        </th>
-                        <th><Text size="md" weight={700}>Product </Text></th>
-                        <th><Center><Text size="md" weight={700}> Price </Text> </Center></th>
-                        <th><Center><Text size="md" weight={700}> Quantity </Text> </Center></th>
-                    </tr>
-                </thead>
-                <tbody>{rows}</tbody>
-            </Table>
+            {
+                !matches ? <Table sx={{ minWidth: 800 }} verticalSpacing="sm">
+                    <thead>
+                        <tr>
+                            <th>
+
+                            </th>
+                            <th><div style={{ marginLeft: '100px' }}><Text size="md" weight={700}>Product </Text></div></th>
+                            <th><Center><Text size="md" weight={700}> Price </Text> </Center></th>
+                            <th><Center><Text size="md" weight={700}> Quantity </Text> </Center></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            productsKey.map((ProductId, ind) => {
+
+                                return (
+                                    <tr key={ind} >
+                                        <td onClick={() => removeItemToCartHandler(ProductId)} style={{ cursor: 'pointer' }}>
+                                            <IconX size={20} stroke={4} color="red" />
+                                        </td>
+                                        <td>
+                                            <Group spacing="sm">
+                                                <LazyLoadImage
+                                                    style={{ borderRadius: '20px' }}
+                                                    width="100px"
+                                                    height="100px"
+                                                    object-fit="cover"
+                                                    radius='md'
+                                                    src={products[ProductId]?.product_image}
+                                                    effect="blur" />
+
+                                                <Text size="sm" weight={700}>
+                                                    {products[ProductId]?.title}
+                                                </Text>
+                                            </Group>
+                                        </td>
+                                        <td><Center> <Text size="sm" weight={700}>{products[ProductId]?.price} </Text> </Center></td>
+                                        <td style={{ width: 140 }}><Center><CartCounter product={products[ProductId]} /> </Center></td>
+                                    </tr>
+                                );
+                            })
+
+                        }</tbody>
+                </Table> :
+
+                    <Product_Cart products={products} />
+            }
+
         </ScrollArea>
     );
 }
