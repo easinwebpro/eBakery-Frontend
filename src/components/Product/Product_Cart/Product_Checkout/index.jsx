1a5130bdf9064ec7b03550f15df939fed3da5a0f
Easin Webpro <easinislam299@gmail.com>
2023-01-09 03:09:17 +0600

    🔥- 💄 [components/products] Product_Checkout  inpubase style rm & dynamic subtotal cost

--- src/components/Product/Product_Cart/Product_Checkout/index.jsx
+++ src/components/Product/Product_Cart/Product_Checkout/index.jsx
@@ -1,34 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Card, Image, Text, Badge, Button, Group, Title, InputBase, Divider } from '@mantine/core';
 import { IconChecks } from '@tabler/icons';
 import Link from 'next/link';
+import { useSelector, useDispatch } from 'react-redux';
+import { calculateTotals } from 'utils/calculateTotals';
+// import { calculateTotals } from 'store/cartSlice';
 
 export const Product_Checkout = () => {
+
+    const [total, setTotal] = useState(0);
+    const [delivery, setDelivery] = useState(60);
+    const products = useSelector((state) => state.cartItems.carts);
+
+
+    useEffect(() => {
+        if (products) {
+            setTotal(calculateTotals(products));
+        }
+    }, [products]);
+
+
+    // console.log('subtotal', subtotal)
+
     return (
         <Card shadow="sm" p="lg" radius="md" withBorder px="md" py="md">
             <Title order={4} weight={700}>CART TOTALS</Title>
 
             <Group position="apart" mt="md" mb="xs">
                 <Text weight={500}>Subtotal:</Text>
-                <Badge color="pink" variant="light">
-                    2,025.00৳
+                <Badge color="pink" variant="light" size="lg">
+                   {Number(total) + Number(delivery)} .00৳
                 </Badge>
             </Group>
             <Divider my="sm" variant="dashed" />
             <Group position="apart" mt="lg" mb="lg">
                 <Text weight={500}>Shipping:</Text>
-                <InputBase radius="md" size="xs" component="select" onChange={(e) => console.log(e.target.value)}
-
-                    styles={{
-                        select: {
-                            label: {
-                                fontWeight: '700 !important',
-                                color: 'red'
-                            }
-                        },
-                    }}
-                >
+                <InputBase radius="md" size="xs" defaultValue={60} component="select" onChange={(e) => setDelivery(e.target.value)} >
                     <option value="60" style={{ fontWeight: 700 }} > InSide of Dhaka </option>
                     <option value="150" style={{ fontWeight: 700 }} > OutSide of Dhaka </option>
                 </InputBase>
@@ -36,7 +44,7 @@ export const Product_Checkout = () => {
 
             <Divider my="sm" variant="dashed" />
             <Link href='/checkout' passHref>
-                <Button leftIcon={<IconChecks size={20} />} fullWidth variant="gradient" gradient={{ from: 'teal', to: 'lime', deg: 105 }} radius='lg' mt="md">
+                <Button leftIcon={<IconChecks size={20} />} fullWidth variant="gradient" gradient={{ from: 'teal', to: 'lime', deg: 105 }} radius='lg' mt="md" disabled={Number(delivery) >= 60 ? false : true}>
                     PROCEED TO CHECKOUT
                 </Button>
             </Link>
